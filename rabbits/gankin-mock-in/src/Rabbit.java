28e72b6223df035bb48c14419cf86823588915d0
Daniel A. Mock <qwertz120@gmail.com>
2017-10-01 22:58:01 +0200

    Restructure, Small Adjustments
    Added required file structure from Assignment
    Defined GrassGrowth as new Grass per Tick

---
+++ rabbits/gankin-mock-in/src/Rabbit.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.awt.Color;
+
+import uchicago.src.sim.gui.Drawable;
+import uchicago.src.sim.gui.SimGraphics;
+import uchicago.src.sim.space.Object2DGrid;
+
+
+
+/**
+ *
+ * @author Dennis
+ */
+public class Rabbit implements Drawable {
+    private int x;
+    private int y;
+    private int vX;
+    private int vY;
+    private Space space;
+    private int energy; 
+    private int grassEnergy;
+    private int birthThreshold;
+    private static int IDNumber=0;
+    private int ID;
+    private int minEng;
+    private int maxEng;
+    //rabbits in total as static var?
+    
+    public Rabbit(int bThreshold,int gEnergy, int maxStartEnergy, int minStartEnergy){
+        x=-1;
+        y=-1;
+        energy= (int) ((Math.random() *(maxStartEnergy-minStartEnergy))+minStartEnergy);
+        birthThreshold=bThreshold;
+        grassEnergy=gEnergy;
+        IDNumber++;
+        ID=IDNumber;
+        minEng=minStartEnergy;
+        maxEng=maxStartEnergy;
+    }
+    
+    public void setXY(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    
+    public void setVXVY(){
+        vX=0;
+        vY=0;
+        while ((vX==0)&&(vY==0)){
+            vX=(int)Math.floor(Math.random()*3)-1;
+            vY=(int)Math.floor(Math.random()*3)-1;
+        }
+    }
+
+    /**
+     * @return the energy
+     */
+    public int getEnergy() {
+        return energy;
+    }
+
+	public void setEnergy(int energy) {
+		this.energy = energy;
+	}
+
+	/**
+     * @return the ID
+     */
+    public String getID() {
+        return "R-"+ID;
+    }
+    
+    public void report(){
+        System.out.println(getID()+" at "+x+", "+y+" has "+getEnergy()+" energy points left.");
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+    
+    public void draw(SimGraphics G){
+        if(energy<5){
+            G.drawFastRoundRect(Color.red);
+        }
+        else{
+        G.drawFastRoundRect(Color.white);
+        }
+    }
+    
+    public void step(){
+        int newX=x+vX;
+        int newY= y+vY;
+        
+        Object2DGrid grid = space.getCurrentRabbitSpace();
+        newX=(newX+grid.getSizeX())%grid.getSizeX();
+        newY=(newY+grid.getSizeY())%grid.getSizeY();
+        
+        if(tryMove(newX,newY)){
+        energy+=space.takeGrassAt(x,y);
+        }
+        else{
+            setVXVY();
+        }
+        energy--;
+    }
+    
+    private boolean tryMove(int newX,int newY){
+        return space.moveRabbitAt(x,y,newX,newY);
+    }
+    
+    public void giveBirth(){
+        energy= (int) ((Math.random() *(maxEng-minEng))+minEng);
+    }
+
+    /**
+     * @param space the space to set
+     */
+    public void setSpace(Space space) {
+        this.space = space;
+    }
+   
+}
---
+++ rabbits/gankin-mock-in/src/RabbitsGrassSimulationModel.java
@@ -0,0 +1,472 @@
+import uchicago.src.sim.engine.Schedule;
+import uchicago.src.sim.engine.SimModelImpl;
+import uchicago.src.sim.engine.SimInit;
+import uchicago.src.sim.gui.DisplaySurface;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import uchicago.src.sim.analysis.DataSource;
+import uchicago.src.sim.analysis.OpenSequenceGraph;
+import uchicago.src.sim.analysis.Sequence;
+import uchicago.src.sim.analysis.BinDataSource;
+import uchicago.src.sim.analysis.OpenHistogram;
+
+import uchicago.src.sim.gui.ColorMap;
+import uchicago.src.sim.gui.Value2DDisplay;
+import uchicago.src.sim.gui.Object2DDisplay;
+import uchicago.src.sim.engine.BasicAction;
+import uchicago.src.sim.util.SimUtilities;
+
+
+/**
+ * Class that implements the simulation model for the rabbits grass
+ * simulation.  This is the first class which needs to be setup in
+ * order to run Repast simulation. It manages the entire RePast
+ * environment and the simulation.
+ *
+ * @author
+ */
+
+
+public class RabbitsGrassSimulationModel extends SimModelImpl {
+
+	public static void main(String[] args) {
+
+		System.out.println("Rabbit start");
+		SimInit init = new SimInit();
+		RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
+		init.loadModel(model, "", false);
+	}
+
+
+	private OpenSequenceGraph amountGrass;
+	private OpenHistogram energyDistr;
+
+
+	public Schedule schedule;
+
+	private Space rabbitSpace;
+
+	private ArrayList rabbitList;
+
+	private DisplaySurface displSurf;
+
+	private int numRabbits = 20;
+	private int worldXSize = 20;
+	private int worldYSize = 20;
+	private int grass = 42;
+	private int grassGrowRate = 15; // New grass per tick
+	private int birthThreshold = 15;
+	private int grassEnergy = 4;
+	private int maxStartEnergy = 12;//energy rabbits are born with
+	private int minStartEnergy = 8;
+
+	/**
+	 * @return the birthThreshold
+	 */
+	public int getBirthThreshold() {
+		return birthThreshold;
+	}
+
+	/**
+	 * @param birthThreshold the birthThreshold to set
+	 */
+	public void setBirthThreshold(int birthThreshold) {
+		this.birthThreshold = birthThreshold;
+	}
+
+
+	//Graphs-------
+	class grassInSpace implements DataSource, Sequence {
+		public Object execute() {
+			return getSValue();
+		}
+
+		public double getSValue() {
+			return (double) rabbitSpace.getTotalGrass();
+		}
+
+	}
+
+	class rabbitsInSpace implements DataSource, Sequence {
+		public Object execute() {
+			return getSValue();
+		}
+
+		public double getSValue() {
+			return (double) rabbitSpace.getTotalRabbits();
+		}
+
+	}
+
+	class rabbitEnergy implements BinDataSource {
+		public double getBinValue(Object o) {
+			Rabbit r = (Rabbit) o;
+			return (double) r.getEnergy();
+		}
+	}
+
+	//------------------------------------
+
+	@Override
+	public void begin() {
+		// TODO Auto-generated method stub
+		buildModel();
+		buildSchedule();
+		buildDisplay();
+
+		displSurf.display();
+		amountGrass.display();
+		energyDistr.display();
+	}
+
+	@Override
+	public String[] getInitParam() {
+		// TODO Auto-generated method stub
+		String[] initParams = {"NumRabbits", "WorldXSize", "WorldYSize", "Money", "grassGrowRate", "grassEnergy", "grass", "birthThreshold"};
+		return initParams;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "RabitsGrassSimulationModel";
+	}
+
+	@Override
+	public Schedule getSchedule() {
+		// TODO Auto-generated method stub
+		return schedule;
+	}
+
+	@Override
+	public void setup() {
+		// TODO Auto-generated method stub
+		System.out.println("setup");
+		rabbitSpace = null;
+		rabbitList = new ArrayList();
+
+		schedule = new Schedule(1);
+
+		if (displSurf != null) {
+			displSurf.dispose();
+		}
+		displSurf = null;
+
+		if (amountGrass != null) {
+			amountGrass.dispose();
+		}
+		amountGrass = null;
+
+		if (energyDistr != null) {
+			energyDistr.dispose();
+		}
+		energyDistr = null;
+
+
+		displSurf = new DisplaySurface(this, "RabbitsGrassModel Window 1");
+		amountGrass = new OpenSequenceGraph("Amount of Grass in Space", this);
+		energyDistr = new OpenHistogram("Rabbit Energy", 8, 0);
+
+
+		registerDisplaySurface("RabbitsGrassModel Window 1", displSurf);
+		this.registerMediaProducer("Plot", amountGrass);
+	}
+
+	public void buildModel() {
+		System.out.println("build Model");
+		rabbitSpace = new Space(worldXSize, worldYSize, grassEnergy);
+		rabbitSpace.growGrass(grass);
+
+		for (int i = 0; i < numRabbits; i++) {
+			addNewRabbit();
+		}
+
+		for (int i = 0; i < rabbitList.size(); i++) {
+			Rabbit r = (Rabbit) rabbitList.get(i);
+			r.report();
+		}
+
+	}
+
+	public void buildSchedule() {
+		System.out.println("build Schedule");
+
+		class CarryDropStep extends BasicAction {
+			public void execute() {
+				SimUtilities.shuffle(rabbitList);
+				for (int i = 0; i < rabbitList.size(); i++) {
+					Rabbit r = (Rabbit) rabbitList.get(i);
+					r.step();
+				}
+
+				int deadRabbits = reapDeadRabbits();
+				for (int i = 0; i < deadRabbits; i++) {
+					//addNewRabbit();
+				}
+
+
+				displSurf.updateDisplay();
+			}
+		}
+
+		schedule.scheduleActionBeginning(0, new CarryDropStep());
+
+		class CarryDropCountLiving extends BasicAction {
+			public void execute() {
+				countLivingRabbits();
+			}
+		}
+		class CarryDropCountBirth extends BasicAction {
+			public void execute() {
+				System.out.println(giveBirth() + " rabbits born");
+			}
+		}
+
+		class CarryDropCountGrass extends BasicAction {
+			private int grassAmount = 1;
+
+			public CarryDropCountGrass(int grassAmount) {
+				this.grassAmount = grassAmount;
+			}
+
+			public void execute() {
+				rabbitSpace.growGrass(grassAmount);
+			}
+		}
+
+		class CarryDropPlotGrass extends BasicAction {
+			public void execute() {
+				amountGrass.step();
+			}
+		}
+
+		class CarryDropUpdateEnerDistr extends BasicAction {
+			public void execute() {
+				if (countLivingRabbits() > 0) {
+					energyDistr.step();
+				} else {
+					energyDistr.dispose();
+				}
+			}
+		}
+
+
+		schedule.scheduleActionAtInterval(10, new CarryDropPlotGrass());
+
+		schedule.scheduleActionAtInterval(10, new CarryDropUpdateEnerDistr());
+
+		schedule.scheduleActionAtInterval(10, new CarryDropCountLiving());
+		schedule.scheduleActionAtInterval(5, new CarryDropCountBirth());
+		schedule.scheduleActionAtInterval(1, new CarryDropCountGrass(grassGrowRate));
+
+
+	}
+
+	public void buildDisplay() {
+		System.out.println("build Display");
+
+		ColorMap map = new ColorMap();
+
+		//which colors do i need?
+		map.mapColor(0, Color.black); //background, no grass
+
+		for (int i = 1; i < 16; i++) {
+			map.mapColor(i, Color.green);
+		}
+
+
+		Value2DDisplay displayGrass = new Value2DDisplay(rabbitSpace.getCurrentGrassSpace(), map);
+
+		Object2DDisplay displayRabbits = new Object2DDisplay(rabbitSpace.getCurrentRabbitSpace());
+		displayRabbits.setObjectList(rabbitList);
+
+		displSurf.addDisplayable(displayGrass, "Grass");
+		displSurf.addDisplayable(displayRabbits, "Rabbits");
+
+		amountGrass.addSequence("Amount of Grass", new grassInSpace());
+		amountGrass.addSequence("Amount of Rabbits", new rabbitsInSpace());
+		energyDistr.createHistogramItem("Rabbit Energy", rabbitList, new rabbitEnergy());
+	}
+
+	public void addNewRabbit() {
+		Rabbit a = new Rabbit(getBirthThreshold(), grassEnergy, getMinStartEnergy(), getMaxStartEnergy());
+
+		if (rabbitSpace.addRabbit(a)) {
+			rabbitList.add(a);
+		}
+	}
+
+	private int reapDeadRabbits() {
+		int count = 0;
+		for (int i = (rabbitList.size() - 1); i >= 0; i--) {
+			Rabbit r = (Rabbit) rabbitList.get(i);
+			if (r.getEnergy() < 1) {
+				rabbitSpace.removeRabbitAt(r.getX(), r.getY());
+				rabbitList.remove(i);
+				count++;
+			}
+		}
+		System.out.println("removed " + count);
+		return count++;
+	}
+
+	private int giveBirth() {
+		int addRabbits = 0;
+		for (int i = 0; i < rabbitList.size(); i++) {
+			Rabbit r = (Rabbit) rabbitList.get(i);
+			if (r.getEnergy() > getBirthThreshold()) {
+				addRabbits++;
+				r.setEnergy(minStartEnergy);
+			}
+		}
+		for (int i = 0; i < addRabbits; i++) {
+			Rabbit r = (Rabbit) rabbitList.get(i);
+			addNewRabbit();
+			r.giveBirth();
+		}
+
+		return addRabbits;
+	}
+
+	private int countLivingRabbits() {
+		int livingRabbits = 0;
+		for (int i = 0; i < rabbitList.size(); i++) {
+			Rabbit r = (Rabbit) rabbitList.get(i);
+			if (r.getEnergy() > 0) {
+				livingRabbits++;
+			}
+		}
+		System.out.println("livng rabbits: " + livingRabbits);
+		return livingRabbits;
+	}
+
+	/**
+	 * @return the numAgents
+	 */
+	public int getNumRabbits() {
+		return numRabbits;
+	}
+
+	/**
+	 * @param numRabbits the numAgents to set
+	 */
+	public void setNumRabbits(int numRabbits) {
+		this.numRabbits = numRabbits;
+	}
+
+	/**
+	 * @return the worldXSize
+	 */
+	public int getWorldXSize() {
+		return worldXSize;
+	}
+
+	/**
+	 * @param worldXSize the worldXSize to set
+	 */
+	public void setWorldXSize(int worldXSize) {
+		this.worldXSize = worldXSize;
+	}
+
+	/**
+	 * @return the worldYSize
+	 */
+	public int getWorldYSize() {
+		return worldYSize;
+	}
+
+	/**
+	 * @param worldYSize the worldYSize to set
+	 */
+	public void setWorldYSize(int worldYSize) {
+		this.worldYSize = worldYSize;
+	}
+
+	/**
+	 * @return the grass
+	 */
+	public int getGrass() {
+		return grass;
+	}
+
+	/**
+	 * @param grass the grass to set
+	 */
+	public void setGrass(int grass) {
+		this.grass = grass;
+	}
+
+	/**
+	 * @return the grassGrowRate
+	 */
+	public int getGrassGrowRate() {
+		return grassGrowRate;
+	}
+
+	/**
+	 * @param grassGrowRate the grassGrowRate to set
+	 */
+	public void setGrassGrowRate(int grassGrowRate) {
+		this.grassGrowRate = grassGrowRate;
+	}
+
+	/**
+	 * @return the birthTreshold
+	 */
+	public int getBirthTreshold() {
+		return getBirthThreshold();
+	}
+
+	/**
+	 * @param birthTreshold the birthTreshold to set
+	 */
+	public void setBirthTreshold(int birthTreshold) {
+		this.setBirthThreshold(birthTreshold);
+	}
+
+	/**
+	 * @return the grassEnergy
+	 */
+	public int getGrassEnergy() {
+		return grassEnergy;
+	}
+
+	/**
+	 * @param grassEnergy the grassEnergy to set
+	 */
+	public void setGrassEnergy(int grassEnergy) {
+		this.grassEnergy = grassEnergy;
+	}
+
+	/**
+	 * @return the maxStartEnergy
+	 */
+	public int getMaxStartEnergy() {
+		return maxStartEnergy;
+	}
+
+	/**
+	 * @param maxStartEnergy the maxStartEnergy to set
+	 */
+	public void setMaxStartEnergy(int maxStartEnergy) {
+		this.maxStartEnergy = maxStartEnergy;
+	}
+
+	/**
+	 * @return the minStartEnergy
+	 */
+	public int getMinStartEnergy() {
+		return minStartEnergy;
+	}
+
+	/**
+	 * @param minStartEnergy the minStartEnergy to set
+	 */
+	public void setMinStartEnergy(int minStartEnergy) {
+		this.minStartEnergy = minStartEnergy;
+	}
+
+
+}
